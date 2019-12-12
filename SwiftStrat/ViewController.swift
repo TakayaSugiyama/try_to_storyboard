2abe250e21647035cc9ee7e1642adbeda843fd31
takayasugiyama <s.takaya1027@gmail.com>
2019-12-12 18:25:37 +0900

    try storyboard

---
+++ SwiftStrat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftStrat
+//
+//  Created by TakayaSugiyama on 2019/12/12.
+//  Copyright © 2019 TakayaSugiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
